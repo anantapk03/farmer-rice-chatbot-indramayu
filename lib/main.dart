c38606833836a1f6d6eee8b878cf9ada2c8378ea
anantapk03 <anantapk03@gmail.com>
2025-01-09 01:01:52 +0700

    Implementasi Firebase Cloud Messaging

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,43 @@ import 'package:base_project_pelatihan_mobile_intermediate_polindra/components/c
 import 'package:base_project_pelatihan_mobile_intermediate_polindra/components/sevices/app_service.dart';
 import 'package:base_project_pelatihan_mobile_intermediate_polindra/components/util/storage_util.dart';
 import 'package:base_project_pelatihan_mobile_intermediate_polindra/notification_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
+
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final NotificationService notificationService = NotificationService();
   await notificationService.initNotification();
   await _dependencyInjection();
+  await _notificationConfiguration();
   runApp(const MyApp());
+}
+
+Future<void> _notificationConfiguration() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    Logger().i("Izin Notifikasi diberikan");
+  } else {
+    Logger().i('Izin notifikasi ditolak');
+  }
+  messaging.getToken().then((String? token) {
+    if (token != null) {
+      Logger().i('FCM Token: $token');
+    }
+  });
 }
 
 /// ====== DI Section =====
